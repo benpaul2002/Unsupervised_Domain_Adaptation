f956abf118f24a9d405e746f0d9b3488e59c099f
benpaul2002 <benpaul2002@gmail.com>
2023-11-27 21:23:18 +0530

    yet another update

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torchvision.models as models
 
 class MNIST_MNISTM(nn.Module):
     def __init__(self):
@@ -76,4 +77,40 @@ class SVHN_MNIST(nn.Module):
         features = features.view(x.shape[0], -1)
         logits = self.classifier(features)
         return logits
+    
+class Office(nn.Module):
+    def __init__(self, num_classes=31):
+        super(Office, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        return x
 
